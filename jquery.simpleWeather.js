f6b71903b202c60c9a30178234e17d39af8a3632
fleeting <twofivethreetwo@gmail.com>
2010-05-16 17:50:52 -0500

    added forecast, wind, units, atmosphere and cleaned up js

--- jquery.simpleWeather.js
+++ jquery.simpleWeather.js
@@ -15,15 +15,15 @@
 	$.extend({
 		simpleWeather: function(options){
 			var options = $.extend({
-				locationId: '76309',
+				location: '76309',
 				tempUnit: 'f',
 				success: function(weather){},
 				error: function(message){}
 			}, options);
 
 			var weatherUrl = 'http://query.yahooapis.com/v1/public/yql?format=json&diagnostics=true&callback=&q=';
-			if(options.locationId != '')
-				weatherUrl += 'select * from weather.forecast where location in ("'+options.locationId+'") and u="'+options.tempUnit+'"';
+			if(options.location != '')
+				weatherUrl += 'select * from weather.forecast where location in ("'+options.location+'") and u="'+options.tempUnit+'"';
 			else {
 				options.error("No location given.");
 				return false;
@@ -47,21 +47,40 @@
 							
 							weather["title"] = result.item.title;
 							weather["temp"] = result.item.condition.temp;
-							weather["unit"] = result.units.temperature;
-							weather["info"] = result.item.condition.text;
-							weather["todayHigh"] = result.item.forecast[0].high;
-							weather["todayLow"] = result.item.forecast[0].low;
-							weather["todayInfo"] = result.item.forecast[0].text;
-							weather["tomorrowHigh"] = result.item.forecast[1].high;
-							weather["tomorrowLow"] = result.item.forecast[1].low;
+							weather["units"] = [];
+							weather["units"]["temp"] = result.units.temperature;
+							weather["units"]["distance"] = result.units.distance;
+							weather["units"]["pressure"] = result.units.pressure;
+							weather["units"]["speed"] = result.units.speed;
+							weather["currently"] = result.item.condition.text;
+							weather["high"] = result.item.forecast[0].high;
+							weather["low"] = result.item.forecast[0].low;
+							weather["forecast"] = result.item.forecast[0].text;
+							weather["wind"] = [];
+							weather["wind"]["chill"] = result.wind.chill;
+							weather["wind"]["direction"] = result.wind.direction;
+							weather["wind"]["speed"] = result.wind.speed;
+							weather["humidity"] = result.atmosphere.humidity;
+							weather["pressure"] = result.atmosphere.pressure;
+							weather["rising"] = result.atmosphere.rising;
+							weather["visibility"] = result.atmosphere.visibility;
+							weather["sunrise"] = result.astronomy.sunrise;
+							weather["sunset"] = result.astronomy.sunset;
+							weather["description"] = result.item.description;
 							weather["image"] = "http://l.yimg.com/a/i/us/nws/weather/gr/"+result.item.condition.code+timeOfDay+".png";
+							
+							weather["tomorrow"] = [];
+							weather["tomorrow"]["high"] = result.item.forecast[1].high;
+							weather["tomorrow"]["low"] = result.item.forecast[1].low;
+							weather["tomorrow"]["forecast"] = result.item.forecast[1].text;
+							weather["tomorrow"]["date"] = result.item.forecast[1].date;
+							weather["tomorrow"]["day"] = result.item.forecast[1].day;
+							weather["tomorrow"]["image"] = "http://l.yimg.com/a/i/us/nws/weather/gr/"+result.item.forecast[1].code+"d.png";
+							
 							weather["city"] = result.location.city;
 							weather["country"] = result.location.country;
 							weather["region"] = result.location.region;
 							weather["updated"] = result.item.pubDate;
-							weather["sunrise"] = result.astronomy.sunrise;
-							weather["sunset"] = result.astronomy.sunset;
-							weather["description"] = result.item.description;
 							weather["link"] = result.item.link;
 						});
 						
