800ecf245438ceb7c665ad9d854cb0fcaa74885a
fleeting <twofivethreetwo@gmail.com>
2010-05-16 18:08:36 -0500

    code cleanup

--- jquery.simpleWeather.js
+++ jquery.simpleWeather.js
@@ -34,8 +34,6 @@
 				dataType: 'json',
 				success: function(data) {
 					if(data != null) {
-						var weather = [];
-						
 						$.each(data.query.results, function(i, result) {
 							currentDate = new Date();
 							sunRise = new Date(currentDate.toDateString() +' '+ result.astronomy.sunrise);
@@ -45,46 +43,49 @@
 							else
 								timeOfDay = 'n';
 							
-							weather["title"] = result.item.title;
-							weather["temp"] = result.item.condition.temp;
-							weather["units"] = [];
-							weather["units"]["temp"] = result.units.temperature;
-							weather["units"]["distance"] = result.units.distance;
-							weather["units"]["pressure"] = result.units.pressure;
-							weather["units"]["speed"] = result.units.speed;
-							weather["currently"] = result.item.condition.text;
-							weather["high"] = result.item.forecast[0].high;
-							weather["low"] = result.item.forecast[0].low;
-							weather["forecast"] = result.item.forecast[0].text;
-							weather["wind"] = [];
-							weather["wind"]["chill"] = result.wind.chill;
-							weather["wind"]["direction"] = result.wind.direction;
-							weather["wind"]["speed"] = result.wind.speed;
-							weather["humidity"] = result.atmosphere.humidity;
-							weather["pressure"] = result.atmosphere.pressure;
-							weather["rising"] = result.atmosphere.rising;
-							weather["visibility"] = result.atmosphere.visibility;
-							weather["sunrise"] = result.astronomy.sunrise;
-							weather["sunset"] = result.astronomy.sunset;
-							weather["description"] = result.item.description;
-							weather["image"] = "http://l.yimg.com/a/i/us/nws/weather/gr/"+result.item.condition.code+timeOfDay+".png";
+							var weather = {							
+								title: result.item.title,
+								temp: result.item.condition.temp,
+								units:{
+									temp: result.units.temperature,
+									distance: result.units.distance,
+									pressure: result.units.pressure,
+									speed: result.units.speed
+								},
+								currently: result.item.condition.text,
+								high: result.item.forecast[0].high,
+								low: result.item.forecast[0].low,
+								forecast: result.item.forecast[0].text,
+								wind:{
+									chill: result.wind.chill,
+									direction: result.wind.direction,
+									speed: result.wind.speed
+								},
+								humidity: result.atmosphere.humidity,
+								pressure: result.atmosphere.pressure,
+								rising: result.atmosphere.rising,
+								visibility: result.atmosphere.visibility,
+								sunrise: result.astronomy.sunrise,
+								sunset: result.astronomy.sunset,
+								description: result.item.description,
+								image: "http://l.yimg.com/a/i/us/nws/weather/gr/"+result.item.condition.code+timeOfDay+".png",
+								tomorrow:{
+									high: result.item.forecast[1].high,
+									low: result.item.forecast[1].low,
+									forecast: result.item.forecast[1].text,
+									date: result.item.forecast[1].date,
+									day: result.item.forecast[1].day,
+									image: "http://l.yimg.com/a/i/us/nws/weather/gr/"+result.item.forecast[1].code+"d.png",
+								},
+								city: result.location.city,
+								country: result.location.country,
+								region: result.location.region,
+								updated: result.item.pubDate,
+								link: result.item.link,
+							};
 							
-							weather["tomorrow"] = [];
-							weather["tomorrow"]["high"] = result.item.forecast[1].high;
-							weather["tomorrow"]["low"] = result.item.forecast[1].low;
-							weather["tomorrow"]["forecast"] = result.item.forecast[1].text;
-							weather["tomorrow"]["date"] = result.item.forecast[1].date;
-							weather["tomorrow"]["day"] = result.item.forecast[1].day;
-							weather["tomorrow"]["image"] = "http://l.yimg.com/a/i/us/nws/weather/gr/"+result.item.forecast[1].code+"d.png";
-							
-							weather["city"] = result.location.city;
-							weather["country"] = result.location.country;
-							weather["region"] = result.location.region;
-							weather["updated"] = result.item.pubDate;
-							weather["link"] = result.item.link;
+							options.success(weather);
 						});
-						
-						options.success(weather);
 					} else {
 						options.error("Bad request.");
 					}
