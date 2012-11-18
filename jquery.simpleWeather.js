6b002f0c5c4c4be709957f040c0b33926cb6b0b8
James Fleeting <twofivethreetwo@gmail.com>
2012-11-18 15:00:57 -0600

    Added WOEID to simpleWeather, version now 2.1.0, and updated included demo.

--- jquery.simpleWeather.js
+++ jquery.simpleWeather.js
@@ -1,39 +1,42 @@
 /*
- * simpleWeather
- * 
- * A simple jQuery plugin to display the weather information
- * for a location. Weather is pulled from the public Yahoo!
- * Weather feed via their api.
+ * simpleWeather.js
+ * http://simpleweather.monkeecreate.com
  *
- * Developed by James Fleeting <hello@jamesfleeting.com>
+ * A simple jQuery plugin to display the current weather
+ * information for any location using Yahoo! Weather.
+ *
+ * Developed by James Fleeting <@twofivethreetwo>
  * Another project from monkeeCreate <http://monkeecreate.com>
  *
- * Version 2.0.1 - Last updated: January 26 2012
+ * Version 2.1 - Last updated: November 17 2012
  */
 (function($) {
 	"use strict";
 	$.extend({
 		simpleWeather: function(options){
 			options = $.extend({
-				zipcode: '76309',
+				zipcode: '',
+				woeid: '2357536',
 				location: '',
 				unit: 'f',
 				success: function(weather){},
 				error: function(message){}
 			}, options);
-			
+
 			var now = new Date();
-			
+
 			var weatherUrl = 'http://query.yahooapis.com/v1/public/yql?format=json&rnd='+now.getFullYear()+now.getMonth()+now.getDay()+now.getHours()+'&diagnostics=true&callback=?&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&q=';
 			if(options.location !== '') {
 				weatherUrl += 'select * from weather.forecast where location in (select id from weather.search where query="'+options.location+'") and u="'+options.unit+'"';
 			} else if(options.zipcode !== '') {
 				weatherUrl += 'select * from weather.forecast where location in ("'+options.zipcode+'") and u="'+options.unit+'"';
+			} else if(options.woeid !== '') {
+				weatherUrl += 'select * from weather.forecast where woeid='+options.woeid+' and u="'+options.unit+'"';
 			} else {
-				options.error("No location given.");
+				options.error("No location given. Please provide either a US zip code, WOEID or location.");
 				return false;
 			}
-						
+
 			$.getJSON(
 				weatherUrl,
 				function(data) {
@@ -42,33 +45,33 @@
 							if (result.constructor.toString().indexOf("Array") !== -1) {
 								result = result[0];
 							}
-														
+
 							var currentDate = new Date();
 							var sunRise = new Date(currentDate.toDateString() +' '+ result.astronomy.sunrise);
 							var sunSet = new Date(currentDate.toDateString() +' '+ result.astronomy.sunset);
-							
+
 							if(currentDate>sunRise && currentDate<sunSet) {
-								var timeOfDay = 'd'; 
+								var timeOfDay = 'd';
 							} else {
 								var timeOfDay = 'n';
 							}
-							
+
 							var compass = ['N', 'NNE', 'NE', 'ENE', 'E', 'ESE', 'SE', 'SSE', 'S', 'SSW', 'SW', 'WSW', 'W', 'WNW', 'NW', 'NNW', 'N'];
 							var windDirection = compass[Math.round(result.wind.direction / 22.5)];
-							
+
 							if(result.item.condition.temp < 80 && result.atmosphere.humidity < 40) {
 								var heatIndex = -42.379+2.04901523*result.item.condition.temp+10.14333127*result.atmosphere.humidity-0.22475541*result.item.condition.temp*result.atmosphere.humidity-6.83783*(Math.pow(10, -3))*(Math.pow(result.item.condition.temp, 2))-5.481717*(Math.pow(10, -2))*(Math.pow(result.atmosphere.humidity, 2))+1.22874*(Math.pow(10, -3))*(Math.pow(result.item.condition.temp, 2))*result.atmosphere.humidity+8.5282*(Math.pow(10, -4))*result.item.condition.temp*(Math.pow(result.atmosphere.humidity, 2))-1.99*(Math.pow(10, -6))*(Math.pow(result.item.condition.temp, 2))*(Math.pow(result.atmosphere.humidity,2));
 							} else {
 								var heatIndex = result.item.condition.temp;
 							}
-							
+
 							if(options.unit === "f") {
 								var tempAlt = Math.round((5.0/9.0)*(result.item.condition.temp-32.0));
 							} else {
 								var tempAlt = Math.round((9.0/5.0)*result.item.condition.temp+32.0);
 							}
-							
-							var weather = {					
+
+							var weather = {
 								title: result.item.title,
 								temp: result.item.condition.temp,
 								tempAlt: tempAlt,
@@ -114,7 +117,7 @@
 								updated: result.item.pubDate,
 								link: result.item.link
 							};
-							
+
 							options.success(weather);
 						});
 					} else {
@@ -127,6 +130,6 @@
 				}
 			);
 			return this;
-		}		
+		}
 	});
 })(jQuery);
